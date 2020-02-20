2574bbf1e4dcdb84a0302c2d103d621a2ad06c3e
msparks714 <60748164+msparks714@users.noreply.github.com>
2020-02-20 13:56:51 -0500

    Take II

---
+++ js/data.js
@@ -0,0 +1,61 @@
+var bathroomData = [
+{
+"name": "Whole Foods Market",
+"type_of_place": "Grocery Store"
+"longitude":-73.9848821,
+"latitude": 40.7546384,
+},
+
+{
+"name": "Whole Foods Market",
+"type_of_place": "Grocery Store"
+"longitude":-73.9981665,
+"latitude": 40.7266796,
+},
+
+{
+"name": "Eataly",
+"type_of_place": "Food Center"
+"longitude":-74.0039172,
+"latitude": 40.7456701,
+},
+
+{
+"name": "Essex Market",
+"type_of_place": "Food Center"
+"longitude":-73.9902573,
+"latitude": 40.7180754,
+},
+
+{
+"name": "Gansevoort Market",
+"type_of_place": "Food Center"
+"longitude":-74.0047896,
+"latitude": 40.732743,
+},
+
+{
+"name": "Gotham West Market",
+"type_of_place": "Food Center"
+"longitude": -73.9968073,
+"latitude": 40.7557629,
+}
+
+{
+"name": "Urbanspace Vanderbilt",
+"type_of_place": "Food Center"
+"longitude": -73.9932024,
+"latitude": 40.7512441,
+}
+
+{
+"name": "NYPL",
+"type_of_place": "Public Library"
+"longitude": -73.9667524,
+"latitude": 40.7962881,
+}
+
+
+
+
+]
